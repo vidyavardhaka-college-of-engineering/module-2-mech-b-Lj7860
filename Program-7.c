ccff88c7c4e6436497462d28e4491826d856c37e
Lj7860 <20me0086@vvce.ac.in>
2021-03-05 07:55:19 +0000

    S

--- Program-7.c
+++ Program-7.c
@@ -8,11 +8,11 @@
 	  int a[10],i,n;
 	  printf("Enter size of the array and the array elements");
 	  scanf("%d",&n);
-    
-	  
-	  for(i=0;i<n;i++)
+	  printf("Enter the array elements");
+    for(i=0;i<n;i++)
 	  scanf("%d",&a[i]);//missing code
 	  for(i=n-1;i>=0;--i)
+	  scanf("%d",&a[i]);
     { //missing code
 	  printf("%d ",a[i]);
     }
